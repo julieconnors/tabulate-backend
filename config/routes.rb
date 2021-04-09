922de2353028232062ce2367b3cf8f5a46fe1e68
julieconnors <juliana.g.connors@gmail.com>
2021-04-08 21:20:11 -0400

    create Horse resources and migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :horses
   resources :owners
   resources :training_options
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
