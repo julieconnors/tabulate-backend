f3108d8dcfde1f38185f4c2d1b6ab5bc8e4d29e0
julieconnors <juliana.g.connors@gmail.com>
2021-04-08 21:21:36 -0400

    create Ride resources and migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :rides
   resources :horses
   resources :owners
   resources :training_options
---
+++ db/migrate/20210409012045_create_rides.rb
@@ -0,0 +1,11 @@
+class CreateRides < ActiveRecord::Migration[6.0]
+  def change
+    create_table :rides do |t|
+      t.string :date
+      t.references :horse, null: false, foreign_key: true
+      t.references :training_option, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
