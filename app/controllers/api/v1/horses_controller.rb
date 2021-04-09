46f0c164b74ab67c95aba28b87ee5f2fe3519d9d
julieconnors <juliana.g.connors@gmail.com>
2021-04-09 06:45:51 -0400

    set up horse serializer

--- app/controllers/api/v1/horses_controller.rb
+++ app/controllers/api/v1/horses_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::HorsesController < ApplicationController
 
     def index
         horses = Horse.all
-        render json: horses
+        render json: HorseSerializer.new(horses)
     end
 end
