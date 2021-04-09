bf80a9f14bad1eff740005ec0090ca2f86b4e307
julieconnors <juliana.g.connors@gmail.com>
2021-04-08 21:23:55 -0400

    namespace routes and controller files

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  resources :rides
-  resources :horses
-  resources :owners
-  resources :training_options
+  namespace :api do
+    namespace :v1 do
+      resources :rides
+      resources :horses
+      resources :owners
+      resources :training_options
+    end
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
