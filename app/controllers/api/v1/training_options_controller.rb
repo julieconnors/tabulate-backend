70b92d379a8df018f3570a35df487726cef3e31c
julieconnors <juliana.g.connors@gmail.com>
2021-04-09 06:55:01 -0400

    setup controller with serialized data

--- app/controllers/api/v1/training_options_controller.rb
+++ app/controllers/api/v1/training_options_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::TrainingOptionsController < ApplicationController
 
     def index
         training_options = TrainingOption.all
-        render json: training_options
+        render json: TrainingOptionSerializer.new(training_options)
     end
 end
