e752447692cf2ecbcbe2622607e5d021e0ccb22b
julieconnors <juliana.g.connors@gmail.com>
2021-04-10 15:39:23 -0400

    add strong params

--- app/controllers/api/v1/rides_controller.rb
+++ app/controllers/api/v1/rides_controller.rb
@@ -4,4 +4,10 @@ class Api::V1::RidesController < ApplicationController
         rides = Ride.all
         render json: RideSerializer.new(rides)
     end
+
+    private
+
+    def ride_params
+        params.require(:ride).permit(:date, :horse_id, :training_option_id)
+    end
 end
