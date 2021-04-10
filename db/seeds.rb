a2c771a2a1a87dab18a8da2454bd16b2062aaa29
julieconnors <juliana.g.connors@gmail.com>
2021-04-10 15:43:17 -0400

    update seed file with additional data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,13 +10,23 @@ Owner.destroy_all
 Horse.destroy_all
 Ride.destroy_all
 
-TrainingOption.create(label: "flat ride", fee: 50)
-TrainingOption.create(label: "show ride", fee: 75)
+TrainingOption.create(label: "flat ride", fee: 30)
+TrainingOption.create(label: "jump school", fee: 50)
+TrainingOption.create(label: "lesson", fee: 75)
+TrainingOption.create(label: "coaching", fee: 125)
+TrainingOption.create(label: "show ride", fee: 100)
 
 Owner.create(name: "Gilda Piccoli")
+Owner.create(name: "Jane Fisher")
+Owner.create(name: "Pat Fulchino")
+Owner.create(name: "Tina Allen")
 
 Horse.create(name: "Ari", owner_id: 1)
-Horse.create(name: "Hans", owner_id: 1)
+Horse.create(name: "Casper", owner_id: 2)
+Horse.create(name: "Justin", owner_id: 2)
+Horse.create(name: "Uber", owner_id: 3)
+Horse.create(name: "Enzo", owner_id: 3)
+Horse.create(name: "Sonny", owner_id: 4)
 
 Ride.create(date: "4-8-21", horse_id: 1, training_option_id: 2)
 Ride.create(date: "4-7-21", horse_id: 2, training_option_id: 1)
