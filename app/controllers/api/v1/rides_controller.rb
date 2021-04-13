a1df5d33ea2f7759ab1558994264583978288d27
julieconnors <juliana.g.connors@gmail.com>
2021-04-13 11:54:39 -0400

    add create action

--- app/controllers/api/v1/rides_controller.rb
+++ app/controllers/api/v1/rides_controller.rb
@@ -5,6 +5,13 @@ class Api::V1::RidesController < ApplicationController
         render json: RideSerializer.new(rides)
     end
 
+    def create
+        ride = Ride.new(ride_params)
+        if ride.save
+            render json: RideSerializer.new(ride)
+        end
+    end
+
     private
 
     def ride_params
