2e5b1be030176beafc3935f90d5827ab2b920bf0
julieconnors <juliana.g.connors@gmail.com>
2021-04-14 06:39:31 -0400

    update ride serializer to include horse and training option

--- app/serializers/ride_serializer.rb
+++ app/serializers/ride_serializer.rb
@@ -1,4 +1,4 @@
 class RideSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :date, :horse_id, :training_option_id
+  attributes :date, :horse_id, :training_option_id, :horse, :training_option
 end
