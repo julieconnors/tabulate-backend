454d28a70db629e1dcb898c0e3e8579efcc6f25b
julieconnors <juliana.g.connors@gmail.com>
2021-04-17 19:51:56 -0400

    add create owner action

--- app/controllers/api/v1/owners_controller.rb
+++ app/controllers/api/v1/owners_controller.rb
@@ -4,4 +4,19 @@ class Api::V1::OwnersController < ApplicationController
         owners = Owner.all
         render json: OwnerSerializer.new(owners)
     end
+
+    def create
+        owner = Owner.new(owner_params)
+        if owner.save
+            render json: OwnerSerializer.new(owner)
+        else
+            render json: { error: "Error creating Owner"}
+        end
+    end
+
+    private
+
+    def owner_params
+        params.require(:owner).permit(:name)
+    end
 end
