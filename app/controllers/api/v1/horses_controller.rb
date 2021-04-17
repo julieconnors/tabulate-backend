3e2adb77112f6de0a0f17222668bab0ed8546073
julieconnors <juliana.g.connors@gmail.com>
2021-04-17 19:51:40 -0400

    add create horse action

--- app/controllers/api/v1/horses_controller.rb
+++ app/controllers/api/v1/horses_controller.rb
@@ -4,4 +4,23 @@ class Api::V1::HorsesController < ApplicationController
         horses = Horse.all
         render json: HorseSerializer.new(horses)
     end
+
+    def create
+        owner = Owner.find_or_create_by(name: params[:owner])
+        binding.pry
+        horse = Horse.new(name: params[:name], owner_id: owner.id)
+        if horse.save
+            render json: HorseSerializer.new(horse)
+        else
+            render json: { error: "Error creating HOrse"}
+        end
+
+
+    end
+
+    private 
+
+    def horse_params
+        params.require(:horse).permit(:name, :owner_id)
+    end
 end
