703e752447ba784e27a3dbd614f5b796563a29e0
julieconnors <juliana.g.connors@gmail.com>
2021-04-20 15:50:57 -0400

    add create action to training options

--- app/controllers/api/v1/training_options_controller.rb
+++ app/controllers/api/v1/training_options_controller.rb
@@ -4,4 +4,19 @@ class Api::V1::TrainingOptionsController < ApplicationController
         training_options = TrainingOption.all
         render json: TrainingOptionSerializer.new(training_options)
     end
+
+    def create
+        training_option = TrainingOption.new(training_option_params)
+        if training_option.save
+            render json: TrainingOptionSerializer.new(training_option)
+        else
+            render json: { error: "Error creating training option"}
+        end
+    end
+
+    private
+
+    def training_option_params
+        params.require(:training_option).permit(:label, :fee)
+    end
 end
