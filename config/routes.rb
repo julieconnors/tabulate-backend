129f42897de7126f903f87281d8d4efd58003f27
julieconnors <juliana.g.connors@gmail.com>
2021-06-05 19:38:28 -0400

    add user resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   resources :services
   namespace :api do
     namespace :v1 do
@@ -6,6 +7,7 @@ Rails.application.routes.draw do
       resources :horses
       resources :owners
       resources :services
+      resources :users
     end
   end
   
