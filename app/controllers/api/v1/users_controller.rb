320520df0176a5b755f6de34d56e6f4f46a08448
julieconnors <juliana.g.connors@gmail.com>
2021-06-05 20:37:24 -0400

    add create action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,11 @@
-class UsersController < ApplicationController
+class Api::V1::UsersController < ApplicationController
 
     def create
-
+        binding.pry
+        @user = User.new(user_params)
+        if @user.valid?
+            
+        end
     end
 
     private
