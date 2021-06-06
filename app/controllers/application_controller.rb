db7a997969d6766b7f45bfc9de233141fd4ecb93
julieconnors <juliana.g.connors@gmail.com>
2021-06-05 20:36:19 -0400

    add encode and decode token methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+
+    def encode_token(payload)
+        JWT.encode(payload, Figaro.env.stripe_api_key)
+    end
+
+    def decoded_token(token)
+        JWT.decode(token, Figaro.env.stripe_api_key)[0]
+    end
 end
